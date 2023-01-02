ce7825f8d5271c3a0f924fe4ee37cda17cceb453
miguelamoreira <40210256@esmad.ipp.pt>
2023-01-02 16:21:24 +0000

    reorganização dos ficheiros

---
+++ scripts/produtos-esmad.js
@@ -0,0 +1,44 @@
+function navbar() {
+    var x = document.getElementById("navbar");
+    if (x.className === "navbar") {
+      x.className += " responsive";
+    } else {
+      x.className = "navbar";
+    }
+}
+
+let contagem = 0;
+let carrinho = {};
+
+if (localStorage.getItem("contagem")) {
+    contagem = parseInt(localStorage.getItem("contagem"));
+}
+
+updateCarrinho();
+
+let btns = document.querySelectorAll(".row-produtos button");
+
+for (let i = 0; i < btns.length; i++) {
+    let btn = btns[i];
+    btn.addEventListener("click", add);
+}
+
+function add(event) {
+    let id = event.target.dataset.id;
+
+if (id in carrinho) {
+    carrinho[id].quant++;
+} else {
+    let carrinhoItem = {
+        quant: 1
+    };
+    carrinho[id] = carrinhoItem
+}
+
+    contagem++;
+    updateCarrinho();
+}
+
+function updateCarrinho() {
+    document.getElementById("contagem").textContent = contagem;
+}
